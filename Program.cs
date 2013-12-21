1ea10ad676c17c1d0ad102050be5188bc79f2455
Adam Baxter <voltagex@voltagex.org>
2013-12-21 21:44:32 +1100

    Add OpenRepo - a hack to handle github-windows:// URLs with SourceTree

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0].Contains("register-github"))
+            {
+                RegistryKey fakeHubRegistry = Registry.CurrentUser.OpenSubKey("Software\\Classes",true).CreateSubKey("github-windows");
+                fakeHubRegistry.SetValue("", "URL:Github for Windows");
+                fakeHubRegistry.SetValue("URL Protocol", "");
+
+                //RegistryKey defaultIcon = helpDesk.CreateSubKey("DefaultIcon");
+                //defaultIcon.SetValue("", Path.GetFileName(Application.ExecutablePath));
+
+                RegistryKey shell = fakeHubRegistry.CreateSubKey("shell");
+                RegistryKey open = shell.CreateSubKey("open");
+                RegistryKey command = open.CreateSubKey("command");
+                command.SetValue("", System.Reflection.Assembly.GetEntryAssembly().Location + " %1");
+            }
+            if (!args[0].StartsWith("github-windows://"))
+            {
+                Environment.Exit(1);
+            }
+
+            else
+            {
+                //for example, github-windows://openRepo/https://github.com/voltagex/junkcode
+               var split = args[0].ToLowerInvariant().Split(new string[] { "github-windows://openrepo/", }, StringSplitOptions.RemoveEmptyEntries); //there has to be a nicer way to do this
+               Process.Start(new ProcessStartInfo() {UseShellExecute = true,FileName="sourcetree://cloneRepo/"+split[0]});
+            }
+
+
+        }
+    }
+}
